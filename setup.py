c82bc782bfcac37cab86513434ab9c4b1ca9f4f5
SuxrobGM <suxrobGM@gmail.com>
2018-07-18 14:14:21 +0500

    changed name and setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,17 @@
 from setuptools import setup
 
-setup(name='funniest',
+setup(name='clausewitz_script',
       version='0.1',
-      description='The funniest joke in the world',
-      url='http://github.com/storborg/funniest',
-      author='Flying Circus',
-      author_email='flyingcircus@example.com',
-      license='MIT',
-      packages=['funniest'],
+      description='Additional python library for scripting of Clausewitz engine games like Hearts of Iron IV, Europa Universalis IV, Crusader King II, Stellaris',
+      url='https://github.com/suxrobGM/ClausewitzScripting',
+      author='SuxrobGM',
+      author_email='suxrobGM@gmail.com',
+      license='Apache 2.0',
+      packages=['clausewitz_script'],
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: Apache 2.0 License',
+        'Programming Language :: Python :: 3.6',
+      ],
+      keywords='Hearts of Iron IV Clausewitz modding',
       zip_safe=False)
